374429792439784ea86dbbbb98767d101ba48cdb
Devin Bidwell <dbidwell94@gmail.com>
2022-10-26 20:55:28 -0700

    Beginning of the request body builder

--- src/theme.tsx
+++ src/theme.tsx
@@ -11,6 +11,7 @@ import "@fontsource/ubuntu/300.css";
 import "@fontsource/ubuntu/400.css";
 import "@fontsource/ubuntu/500.css";
 import "@fontsource/ubuntu/700.css";
+import "@fontsource/ubuntu-mono";
 
 const globalStyle = css`
   html {
